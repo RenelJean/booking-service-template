f1124160af4be5261213cdd209c01e2f89c86e38
Renel Jean-Baptiste <reneljean123@gmail.com>
2020-11-02 17:23:24 -0500

    Add files via upload
    update code (incomplete)

--- outdoors-website-master/tours/scripts.js
+++ outdoors-website-master/tours/scripts.js
@@ -1,1 +1,12 @@
-//start
+//Function expression to select elements
+const selectElement = (s) => document.querySelector(s);
+
+//open menu on click
+selectElement(".open").addEventListener("click", () => {
+  selectElement(".nav-list").classList.add("active");
+});
+
+//Clses the menu
+selectElement(".close").addEventListener("click", () => {
+  selectElement(".nav-list").classList.remove("active");
+});
